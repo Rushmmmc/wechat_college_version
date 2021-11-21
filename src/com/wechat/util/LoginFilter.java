4b9bef7496e3ca604fae88132e42e4a1237e8428
Ray <1010821512@qq.com>
2021-11-21 22:44:17 +0800

    完成包括登录过滤，验证码的模块开发

---
+++ src/com/wechat/util/LoginFilter.java
@@ -0,0 +1,35 @@
+package com.mywechat.util;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * 过滤器
+ */
+public class LoginFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("../login.jsp");
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
